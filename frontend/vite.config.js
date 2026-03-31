6d864369aa4b2e6e7a52fd3f7be6f10c27d802f9
Abhishek <ghoshabhishek099@gmail.com>
2026-03-31 12:39:23 +0530

    Configured API baseURL and deployment setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
+const API_URL = "https://your-backend.onrender.com";
 export default defineConfig({
   plugins: [react(),tailwindcss()],
 })
